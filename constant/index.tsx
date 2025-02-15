2502eded1f88d6bcfb82c414a1df367e8e7fac5b
T. Landon Love <12stonedesigns@gmail.com>
2025-02-14 18:45:46 -0600

    Update GitHub and LinkedIn profile links

--- constant/index.tsx
+++ constant/index.tsx
@@ -1,11 +1,11 @@
 export const Social_Icons = [
   {
-    link: "https://github.com",
+    link: "https://github.com/12stonedesigns",
     image: "/Github.svg",
     alt: "Tyler Love - GitHub",
   },
   {
-    link: "https://linkedin.com",
+    link: "https://linkedin.com/in/tyler-love-12stone-designs",
     image: "/LinkedIn.svg",
     alt: "Tyler Love - LinkedIn",
   },
