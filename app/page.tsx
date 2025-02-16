b56f4738fe959707938e6ff7da0693ffbb374a57
T. Landon Love <12stonedesigns@gmail.com>
2025-02-16 23:16:25 +0000

    Fix background image path for LooperGroup2.png

--- app/page.tsx
+++ app/page.tsx
@@ -8,17 +8,19 @@ import Websites from "@/components/Websites";
 
 export default function Home() {
   return (
-    <main 
-      className="h-full w-full min-h-screen" 
-      style={{ 
-        backgroundImage: "url('/LooperGroup2.png')", 
-        backgroundRepeat: 'no-repeat', 
-        backgroundSize: 'cover', 
-        backgroundPosition: 'center' 
-      }}
-    >
+    <main className="h-screen w-full relative">
+      <div 
+        className="absolute inset-0 z-[-1]"
+        style={{ 
+          backgroundImage: "url('/LooperGroup2.png')", 
+          backgroundRepeat: 'no-repeat', 
+          backgroundSize: '50%', 
+          backgroundPosition: 'center',
+          opacity: '0.3'
+        }}
+      />
       <Navbar />
-      <div className="flex flex-col gap-20">
+      <div className="flex flex-col gap-20 relative z-10">
         <Banner />
         <About />
         <Experience />
